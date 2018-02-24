072856762b4334fc9fe822c0a1c0bda82a7d8ebd
Charlotte Faye Galarpe <charlottefayeg@gmail.com>
2018-02-24 11:30:43 +0800

    deployment3

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ mongoose.connect(config.uri, (err) => {
 app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(express.static(__dirname + '/client/dist/')); // Provide static directory for frontend
+app.use(express.static(__dirname + '/public')); // '/client/dist/' Provide static directory for frontend
 app.use('/authentication', authentication); // Use Authentication routes in application
 app.use('/blogs', blogs); // Use Blog routes in application
 
 
 // Connect server to Angular 2 Index.html
 app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname + '/client/index.html'));
+ res.sendFile(path.join(__dirname + '/public/index.html')); //'/client/index.html'
 });
 
 // Start Server: Listen on port 8080
